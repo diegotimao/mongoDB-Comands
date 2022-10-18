33471fc9ad3dbd09b3f2fb98872a4012f5557f69
diegotimao <diegotimao1104@gmail.com>
2022-10-18 09:29:09 -0500

    feat: Retorna os lanches com caloria abaixo de 500

---
+++ challenges/desafio9.js
@@ -0,0 +1,16 @@
+// Retorne o nome de todos os lanches que possuam calorias abaixo de 500
+
+db.produtos.find(
+  { 
+    valoresNutricionais: { 
+      $elemMatch: {
+        tipo: 'calorias',
+        quantidade: { $lt: 500}
+      }
+    }
+  },
+  {
+    _id: false,
+    nome: true,
+  }
+);
