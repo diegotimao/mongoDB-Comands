a541d9cb40ac9c2ac0003978b62ebfe863033fdb
diegotimao <diegotimao1104@gmail.com>
2022-10-18 09:57:45 -0500

    feat: Produtos não iguais a Big Mac e McChicken

---
+++ challenges/desafio11.js
@@ -0,0 +1,17 @@
+// Retorne o nome do produto, a quantidade de curtidas e quantos itens 
+// foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken
+
+db.produtos.find(
+  {
+    $and: [
+      { nome: { $ne: 'Big Mac'}},
+      { nome: { $ne: 'McChicken'}},
+    ]
+  },
+  {
+    _id: false,
+    nome: true,
+    vendidos: true,
+    curtidas: true,
+  }
+);
