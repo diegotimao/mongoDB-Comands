d609b1df9a68b2f871876854399cb1e0703c342a
diegotimao <diegotimao1104@gmail.com>
2022-10-21 15:49:23 -0500

    feat: Desafio 20

---
+++ challenges/desafio20.js
@@ -0,0 +1,12 @@
+db.produtos.updateOne({ nome: "Quarteirão com Queijo" }, {
+  $pop: { ingredientes: -1 },
+});
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    ingredientes: true,
+  },
+);
