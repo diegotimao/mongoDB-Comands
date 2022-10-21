974808ef76891de0b2b65fdd3b05dde9daa384c0
diegotimao <diegotimao1104@gmail.com>
2022-10-21 16:54:32 -0500

    fix: Correção lint requisito 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -5,13 +5,13 @@ db.produtos.find(
   {
     valoresNutricionais: {
       $elemMatch: { 
-          tipo: 'proteínas',
-          quantidade: { $gt: 20, $lt: 40 }
-        }
-      }
+          tipo: "proteínas",
+          quantidade: { $gt: 20, $lt: 40 },
+        },
+      },
     },
   {
     _id: false,
     nome: true,
-  }
+  },
 );
