820d12a8b9542ff4629e463e3ba7e97b90eb6bb3
diegotimao <diegotimao1104@gmail.com>
2022-10-21 13:50:52 -0500

    feat: Requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,22 @@
+// Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac
+
+db.produtos.updateOne(
+  {
+    name: 'Big Mac'
+  },
+  {
+    $currentDate: {
+      ultimaModificacao: true,
+    }
+  }
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: true },
+  },
+  {
+    _id: false,
+    nome: true
+  }
+)
