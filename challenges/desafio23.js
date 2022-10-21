2a1d7361e1a9e34540f097046949fda18f0c94d7
diegotimao <diegotimao1104@gmail.com>
2022-10-21 17:23:14 -0500

    feat: Requisito 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {},
+  {
+    $push: {
+      tags: {
+        $each: ["combo", "tasty"],
+        $sort: 1,
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+  },
+);
