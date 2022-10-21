5abd520f95885c77efecdb1d5df24746f8e01eb6
diegotimao <diegotimao1104@gmail.com>
2022-10-21 15:49:09 -0500

    feat: Requisito 19

---
+++ challenges/desafio19.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  {},
+  {
+    $pull: {
+      ingredientes: { $eq: "cebola" }
+    }
+    }
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    ingredientes: true,
+  }
+);
