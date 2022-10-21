57c8bccdb71012e4ab90a5a3f77d7a9afd2e2ecb
diegotimao <diegotimao1104@gmail.com>
2022-10-21 16:55:20 -0500

    fix: Correção lint requisito 15

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -6,23 +6,24 @@ db.produtos.updateMany(
   $set: {
     avaliacao: NumberInt(0),
   },
-});
-
-db.produtos.updateMany(
-  { tags: { $all: ['bovino']}},
-  { $inc: {
-    avaliacao: 5,
-    } 
-  }
+},
 );
 
 db.produtos.updateMany(
-  { tags: { $all: ['ave']}},
+  { tags: { $all: ["bovino"] } },
+  { $inc: {
+    avaliacao: 5,
+    }, 
+  },
+);
+
+db.produtos.updateMany(
+  { tags: { $all: ["ave"] } },
   {
     $set: {
       avaliacao: 3,
-    }
-  }
+    },
+  },
 );
 
 db.produtos.find({}, {
