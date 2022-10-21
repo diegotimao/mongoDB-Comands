6dce2df722cf94834c57d0e9e455cafdc85ecb75
diegotimao <diegotimao1104@gmail.com>
2022-10-21 18:37:51 -0500

    feat: Requisito 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,19 @@
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+  {
+    $push: {
+      tags: {
+        $each: ["muito sódio"],
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: false,
+    nome: true,
+    tags: true,
+  },
+);
