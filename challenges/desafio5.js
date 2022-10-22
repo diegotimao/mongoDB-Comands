7090c32efdac8b51e12ffcbbdcddf431c8a46fd7
diegotimao <diegotimao1104@gmail.com>
2022-10-22 11:01:57 -0500

    fix: Correção de lint requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,14 +1,14 @@
 db.produtos.find(
     {
       $or: [
-        {curtidas: { $eq: 36 }},
-        {vendidos: { $eq: 85 }}
-      ]
+        { curtidas: { $eq: 36 } },
+        { vendidos: { $eq: 85 } },
+      ],
    },
   {
     _id: false,
     nome: true,
     curtidas: true,
     vendidos: true,
-  }
+  },
 );
